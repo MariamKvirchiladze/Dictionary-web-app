4ce677286f11d485de4774be547a7ca58bf2f374
MariamKvirchiladze <kvircho555@gmail.com>
2023-07-14 00:04:29 +0400

    add output file and implement audio button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import Header from "./components/default/Header";
 import SearchBar from "./components/default/Search-bar";
+import Output from "./components/output/Output";
+import Types from "./types";
 
 function App() {
   const [darkMode, setDarkMode] = useState<boolean>(false);
   const [font, setFont] = useState<number>(0);
   const [word, setWord] = useState<string>("");
+  const [definition, setDefinition] = useState<Types | null>(null);
   return (
     <div
       className={`w-full min-h-screen px-[24px] pt-[24px] pb-[84px] flex-col flex justify-start items-center gap-[24px] md:px-[40px] md:pt-[58px] md:pb-[118px] md:gap-[51px] lg:pb-[124px] lg:px-[0] ${
@@ -19,6 +22,12 @@ function App() {
         setFont={setFont}
       />
       <SearchBar darkMode={darkMode} word={word} setWord={setWord} />
+      <Output
+        darkMode={darkMode}
+        word={word}
+        definition={definition}
+        setDefinition={setDefinition}
+      />
     </div>
   );
 }
